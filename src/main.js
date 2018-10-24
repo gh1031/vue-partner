71d6a6ab90d857f0d94bae7df6e6b0ffd434dcd6
guanhao <1051421758@qq.com>
2018-10-24 11:10:29 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
-import VuePartner from 'vue-partner';
+// import VuePartner from 'vue-partner';
 import App from './App.vue';
+import Button from './packages/Button.vue';
+import './packages/index.scss';
 
-Vue.use(VuePartner);
+// Vue.use(VuePartner);
 Vue.config.productionTip = false;
+Vue.component(Button.name, Button);
 
 new Vue({
   render: h => h(App),
--- src/packages/Button.vue
+++ src/packages/Button.vue
@@ -22,17 +22,5 @@ export default {
 </script>
 
 <style lang="scss">
-.p-button {
-  outline: 0;
-  padding: 4px 15px;
-  border-radius: 4px;
-  color: #fff;
-  font-weight: 400;
-  border-color: #f5222d;
-  background-color: #f5222d;
-  &:hover {
-    border-color: #ff4d4f;
-    background-color: #ff4d4f;
-  }
-}
+
 </style>
