e76f1ae613a028b28474c118d2301c7be8c9c2b8
guanhao <1051421758@qq.com>
2018-10-24 11:18:20 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue';
-// import VuePartner from 'vue-partner';
 import App from './App.vue';
 import Button from './packages/Button.vue';
 import './packages/index.scss';
 
-// Vue.use(VuePartner);
 Vue.config.productionTip = false;
 Vue.component(Button.name, Button);
 
